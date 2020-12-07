05ec7a5a7462f5651ab89e3407559bdfc1008477
InvenireAudeAdmin <albert.krzymowski@invenireaude.com>
2020-12-07 08:12:28 +0100

    Importing ./IA20-CommonLib/src/ia20/commonlib/config.h

---
+++ IA20-CommonLib/src/ia20/commonlib/config.h
@@ -0,0 +1,31 @@
+/*
+ * File: IA20-CommonLib/src/commonlib/config.h
+ *
+ * Copyright (C) 2015, Albert Krzymowski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef IA20_CONFIG_H_
+#define IA20_CONFIG_H_
+
+
+#define IA20_USE_PTHREADS 	    1
+#define IA20_GNU_STACKTRACE 	    1
+
+#define IA20_FEATURE_THREAD_SAFE 	1
+#define IA20_FEATURE_STACKTRACE 	    1
+
+#define IA20_SHOW_STACK_SIZE 16
+
+
+#endif /* CONFIG_H_ */
