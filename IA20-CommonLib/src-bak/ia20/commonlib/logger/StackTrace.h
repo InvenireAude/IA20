3e4761b9ff002cbcd10367268a64db30cfb138d6
InvenireAudeAdmin <albert.krzymowski@invenireaude.com>
2020-12-07 08:12:28 +0100

    Importing ./IA20-CommonLib/src-bak/ia20/commonlib/logger/StackTrace.h

---
+++ IA20-CommonLib/src-bak/ia20/commonlib/logger/StackTrace.h
@@ -0,0 +1,41 @@
+/*
+ * File: IA20-CommonLib/src/commonlib/logger/StackTrace.h
+ *
+ * Copyright (C) 2015, Albert Krzymowski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/* IA20_COPYRIGHT */
+
+/* ChangeLog:
+ *
+ */
+
+#ifndef _IA20_StackTrace_H_
+#define _IA20_StackTrace_H_
+
+#include <features.h>
+
+namespace IA20{
+
+/*************************************************************************/
+
+
+#ifdef __GLIBC__
+void PrintTrace(std::ostream& os);
+#endif
+
+}
+
+#endif /*STACKTRACE_H_*/
+
