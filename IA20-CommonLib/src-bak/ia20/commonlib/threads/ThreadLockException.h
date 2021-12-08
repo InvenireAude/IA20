6b5a550fab886a476e1651c93d640c901ab25ade
Your Name <InvenireAudeAdmin>
2021-12-08 08:39:11 +0100

    DataModelLib and minor refactor (AK)

--- IA20-CommonLib/src-bak/ia20/commonlib/threads/ThreadLockException.h
+++ IA20-CommonLib/src-bak/ia20/commonlib/threads/ThreadLockException.h
@@ -1,7 +1,7 @@
 /*
  * File: IA20-CommonLib/src/commonlib/threads/ThreadLockException.h
  *
- * Copyright (C) 2015, Albert Krzymowski
+ * Copyright (C) 2021, Albert Krzymowski
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
